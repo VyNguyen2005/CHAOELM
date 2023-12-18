69547d7adeff23a0776a1d0d23b90d35aeb57a68
VyNguyen <nguyenvyadoramy@gmail.com>
2023-12-18 11:15:12 +0700

    version 2

--- Final1/Program.cs
+++ Final1/Program.cs
@@ -11,6 +11,7 @@ namespace Final1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Xin chao!");
             Console.ReadLine();
         }
     }
